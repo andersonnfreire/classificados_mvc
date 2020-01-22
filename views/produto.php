e2da21fb6900d86dc517a2e4d6a8ccc5136c1d1a
Anderson N Freire <coolerinformatica1995@gmail.com>
2020-01-22 11:41:06 -0200

    Ajustando css dos slides e paginação do produto

--- views/produto.php
+++ views/produto.php
@@ -1,27 +1,40 @@
 <div class="container-fluid">
-	<div class="row">
-		<div class="col-sm-5">
-			
-			<div class="carousel slide" data-ride="carousel" id="meuCarousel">
-				<div class="carousel-inner" role="listbox">
-					<?php foreach($info['fotos'] as $chave => $foto): ?>
-					<div class="item <?php echo ($chave=='0')?'active':''; ?>">
-						<img src="<?php echo BASE_URL; ?>assets/images/anuncios/<?php echo $foto['url']; ?>" />
-					</div>
-					<?php endforeach; ?>
-				</div>
-				<a class="left carousel-control" href="#meuCarousel" role="button" data-slide="prev"><span><</span></a>
-				<a class="right carousel-control" href="#meuCarousel" role="button" data-slide="next"><span>></span></a>
-			</div>
+    <div class="row">
+        <div class="col-sm-5">
 
-		</div>
-		<div class="col-sm-7">
-			<h1><?php echo $info['titulo']; ?></h1>
-			<h4><?php echo utf8_encode($info['categoria']); ?></h4>
-			<p><?php echo $info['descricao']; ?></p>
-			<br/>
-			<h3>R$ <?php echo number_format($info['valor'], 2); ?></h3>
-			<h4>Telefone: <?php echo $info['telefone']; ?></h4>
-		</div>
-	</div>
+            <div class="carousel slide" data-ride="carousel" id="meuCarousel">
+
+                <ol class="carousel-indicators">
+                    <li data-target="#meuCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#meuCarousel" data-slide-to="1"></li>
+                    <li data-target="#meuCarousel" data-slide-to="2"></li>
+                </ol>
+
+                <div class="carousel-inner">
+                    <?php foreach ($info['fotos'] as $chave => $foto): ?>
+                        <div class="carousel-item <?php echo ($chave == '0') ? 'active' : ''; ?>" data-interval="10000">
+                            <img src="<?php echo BASE_URL; ?>assets/images/anuncios/<?php echo $foto['url']; ?>" class="d-block w-100" />
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+                <a class="carousel-control-prev" href="#meuCarousel" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#meuCarousel" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+
+        </div>
+        <div class="col-sm-7">
+            <h1><?php echo $info['titulo']; ?></h1>
+            <h4><?php echo utf8_encode($info['categoria']); ?></h4>
+            <p><?php echo $info['descricao']; ?></p>
+            <br/>
+            <h3>R$ <?php echo number_format($info['valor'], 2); ?></h3>
+            <h4>Telefone: <?php echo $info['telefone']; ?></h4>
+        </div>
+    </div>
 </div>
--- views/template.php
+++ views/template.php
@@ -11,7 +11,7 @@ and open the template in the editor.
         <title>Meu site</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/style.css" />
         <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
 
@@ -36,14 +36,14 @@ and open the template in the editor.
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav><br><br><br>
 
 
         <?php $this->loadViewInTemplate($viewName, $viewData); ?>
 
-
-
-        <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
         <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery.min"></script>
         <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/script.js"></script>
 
